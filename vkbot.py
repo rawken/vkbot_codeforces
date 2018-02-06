a9722b751dff78afdc585dec3757a8f8c89c26c0
Roman Kuzmin <romanrus023@gmail.com>
2018-02-06 13:38:34 +0400

    Edit post appearence

--- vkbot.py
+++ vkbot.py
@@ -1,10 +1,16 @@
 import vk
 import json
-import datetime   
+from datetime import datetime   
 import requests
 import time
+import locale
+import pytz
+import collections
+from pytz import timezone
+from pytz import utc
 
-accesstoken = 'accesstoken_sample'
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+accesstoken = ''
 group_id = 
 session = vk.Session(access_token=accesstoken)
 api = vk.API(session)
@@ -12,20 +18,57 @@ i = 0
 with open('contest_ids.json', 'r') as conid:
     a = json.loads(conid.read())
 
-def format_post(a, i):
-    a = [str(reqres['result'][i]['name']),
+def short_tnd_url(i):
+    req_ref = reqres['result'][i]['startTimeSeconds']
+    dt = utc.localize(datetime.utcfromtimestamp(req_ref))
+    dt = dt.astimezone(timezone('Europe/Moscow'))
+    params = collections.OrderedDict()
+    params['day'] = int(dt.strftime('%d'))
+    params['month'] = int(dt.strftime('%m'))
+    params['year'] = dt.strftime('%Y')
+    params['hour'] = dt.strftime('%H')
+    params['min'] = dt.strftime('%M')
+    params['sec'] = int(dt.strftime('%S'))
+    params['p1'] = 166
+    tnd_req = requests.get('https://www.timeanddate.com/worldclock/fixedtime.html?', params)
+    key = ''
+    post_url = 'https://www.googleapis.com/urlshortener/v1/url?key='+key
+    payload = {'longUrl': tnd_req.url}
+    headers = {'content-type': 'application/json'}
+    r = requests.post(post_url, data = json.dumps(payload), headers = headers).text
+    r = json.loads(r)
+    return r['id']
+
+def format_post(post, i):
+    name = str(reqres['result'][i]['name'])
+    if 'Educational' in (reqres['result'][i]['name']):
+        name_emoji = '&#127891;'
+    else:
+        name_emoji = '&#128161;'
+    dur_hours = reqres['result'][i]['durationSeconds'] // 3600
+    dur_minutes = reqres['result'][i]['durationSeconds'] % 3600 // 60
+    if dur_minutes == 0:
+        duration = str(dur_hours) + ':00'
+    else:
+        duration = str(dur_hours) + ':' + str(dur_minutes)
+    start_time_sec = int(reqres['result'][i]['startTimeSeconds'])
+    end_time_sec = int(start_time_sec + reqres['result'][i]['durationSeconds'])
+    start_dt = utc.localize(datetime.utcfromtimestamp(start_time_sec))
+    start_dt = start_dt.astimezone(timezone('Europe/Moscow'))
+    end_dt = utc.localize(datetime.utcfromtimestamp(end_time_sec))
+    end_dt = end_dt.astimezone(timezone('Europe/Moscow'))
+    contest_id = str(reqres['result'][i]['id'])
+    start_date = str(start_dt.strftime('%d.%m.%Y (%a) %H:%M'))
+    end_time = str(end_dt.strftime('%H:%M')) + ' (МСК)'
+    post = [name_emoji + name,
     '\n',
     '\n',
-    'Длительность - ' + str(reqres['result'][i]['durationSeconds'] // 3600),
-    ':',
-    str(reqres['result'][i]['durationSeconds'] % 3600 // 60),
+    '&#9203;' + 'Длительность - ' + duration,
     '\n',
-    'Время контеста - ' + str((
-        datetime.datetime.fromtimestamp(
-            int(reqres['result'][i]['startTimeSeconds'])
-        ).strftime('%d.%m.%Y %H:%M')
-    )) + ' GMT' ]
-    return a
+    '&#128197;' + 'Время контеста - ' + start_date + '-' + end_time,
+    '\n',
+    '&#9888;' + 'Начало в вашем регионе - ' + str(short_url)]
+    return post
 
 while True:
     req = requests.get('http://codeforces.com/api/contest.list?')
@@ -34,7 +77,9 @@ while True:
     phase = reqres['result'][i]['phase']
     contest_id = reqres['result'][i]['id']
     relativeTime = reqres['result'][i]['relativeTimeSeconds']
-    if phase == 'BEFORE' and contest_id not in a and relativeTime >= -172800: 
+    contest_url = 'http://codeforces.com/contests/' + str(contest_id)
+    short_url = short_tnd_url(i)
+    if phase == 'BEFORE' and relativeTime >= -172800 and contest_id not in a: 
         a.append(reqres['result'][i]['id'])
         post = 0
         post = format_post(post, i)
@@ -42,7 +87,8 @@ while True:
         api.wall.post(
             owner_id = group_id, 
             from_group = 1, 
-            message = post
+            message = post,
+            attachments = contest_url
         )
         with open('contest_ids.json', 'a') as conid:
             json.dump(contest_id, conid)
